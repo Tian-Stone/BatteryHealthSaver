99074cf61985a75c6235696906316a9ba32c4cb5
Tian-Stone <142704370+Tian-Stone@users.noreply.github.com>
2024-08-22 09:15:38 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,24 +5,34 @@ int main()
 {
     HWND hWnd = GetConsoleWindow();
     ShowWindow(hWnd, SW_HIDE);
-    MessageBox(NULL, "欢迎使用智能电池健康管理系统！", "电池健康", MB_USERICON | MB_SYSTEMMODAL);
-//	cout << x;
+    MessageBox(NULL, "欢迎使用智能电池健康管理系统！", "电池健康", MB_ICONINFORMATION | MB_SYSTEMMODAL);
+    int ti = 60000;
     while(true)
     {
         LPSYSTEM_POWER_STATUS power = new _SYSTEM_POWER_STATUS();
-	    int ret = GetSystemPowerStatus(power);
+	int ret = GetSystemPowerStatus(power);
         int percent = (int)power->BatteryLifePercent;
         int isCharge = power->ACLineStatus;
         if(percent >= 70 && isCharge == 1)
         {
             MessageBox(NULL, "电量已达到 70%，可停止充电！", "电池健康", MB_ICONINFORMATION | MB_SYSTEMMODAL);
+	    ti = 60000;
         }
         if(percent <= 20 && isCharge == 0)
         {
             MessageBox(NULL, "电量低于 20%，建议开始充电！", "电池健康", MB_ICONINFORMATION | MB_SYSTEMMODAL);
+	    ti = 60000;
         }
+	if(percent >= 65 && isCharge == 1)
+	{
+		ti = 10000;
+	}
+	if(percent <= 25 && isCharge == 0)
+	{
+		ti = 10000;
+	}
         delete power;
-		Sleep(60000);
+	Sleep(ti);
     }
     return 0;
 }
